97e0b9c05a10d4588306c2699c1b96d45af7d35b
maurosoppelsa <l.mauro.soppelsa@gmail.com>
2022-04-07 10:30:51 -0300

    update mongo service

--- src/services/mongo-service.ts
+++ src/services/mongo-service.ts
@@ -5,7 +5,9 @@ class MongoService {
     try {
       //TODO hanlde environment URI
       //TODO configure setTimeOut connection
-      await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/sygthings-db');
+      await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/sygthings-db', {
+        socketTimeoutMS: 2000,
+      });
     } catch (error) {
       console.log(error);
     }
@@ -15,5 +17,9 @@ class MongoService {
     mongoose.connection.close();
   }
 }
+
+mongoose.connection.on('error', err => {
+  console.log(`The server couldn't stablish the connection database`, err);
+});
 
 export default MongoService;
