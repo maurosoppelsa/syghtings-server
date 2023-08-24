ff2ba1f0f2845d7dcea9f6ddda6bd1c716e58e21
maurosoppelsa <l.mauro.soppelsa@gmail.com>
2023-08-24 09:06:42 -0300

    update email service

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -1,11 +1,50 @@
+import nodemailer from 'nodemailer';
+
 class EmailService {
-  public static async sendRegistrationEmail(email, userId, token) {
-    console.log(`Sending registration email to ${email} with token ${token}`);
-    console.log('link: http://localhost:3000/users/verify/' + userId + '/' + token);
+  private transporter;
+
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.SERVER_EMAIL_ACCOUNT,
+        pass: process.env.SERVER_EMAIL_PASSWORD,
+      },
+    });
   }
-  public static async sendResetEmail(email, userId, token) {
-    console.log(`Sending reset email to ${email} with token ${token}`);
-    console.log('link: http://localhost:3000/users/auth-reset-password/' + userId + '/' + token);
+
+  public async sendRegistrationEmail(email, userId, token) {
+    const mailOptions = {
+      from: process.env.SERVER_EMAIL_ACCOUNT,
+      to: email,
+      subject: 'Registration Email',
+      text: `Click the link to verify your account: http://localhost:3000/users/verify/${userId}/${token}`,
+    };
+
+    try {
+      await this.transporter.sendMail(mailOptions);
+      console.log(`Registration email sent to ${email}`);
+    } catch (error) {
+      console.error('Error sending registration email:', error);
+    }
+  }
+
+  public async sendResetEmail(email, userId, token) {
+    const mailOptions = {
+      from: process.env.SERVER_EMAIL_ACCOUNT,
+      to: email,
+      subject: 'Password Reset Email',
+      text: `Click the link to reset your password: http://localhost:3000/users/auth-reset-password/${userId}/${token}`,
+    };
+
+    try {
+      await this.transporter.sendMail(mailOptions);
+      console.log(`Reset email sent to ${email}`);
+    } catch (error) {
+      console.error('Error sending reset email:', error);
+    }
   }
 }
 
